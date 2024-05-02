fa7d9391c03b949fc8a5490dd948c30d0f0b18ac
Abdelrahman M.Ali <abdelrahmansalby23@gmail.com>
2024-05-03 01:19:01 +0300

    uploading simulation of Tiva-C implemented by Saad

--- Milestone_1/Keil Project/main.d
+++ Milestone_1/Keil Project/main.d
@@ -1,4 +1,4 @@
 .\main.o: ..\CODES\main.c
 .\main.o: ..\CODES\STD_TYPES.h
-.\main.o: ..\CODES\DIO_interface.h
-.\main.o: ..\CODES\DIO_private.h
+.\main.o: ..\CODES\interface.h
+.\main.o: ..\CODES\private.h
---
+++ Milestone_1/Keil Project/program.d
@@ -0,0 +1,10 @@
+.\program.o: ..\CODES\program.c
+.\program.o: ..\CODES\interface.h
+.\program.o: ..\CODES\STD_TYPES.h
+.\program.o: ..\CODES\private.h
+.\program.o: ..\CODES\tm4c123gh6pm.h
+.\program.o: ..\CODES\BIT_MATH.h
+.\program.o: C:\Keil\ARM\ARMCC\bin\..\include\math.h
+.\program.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\program.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
+.\program.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
