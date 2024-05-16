30730b7384dfb25539128e6ca409565bd5bff1d2
orabi55 <orabiola55@gmail.com>
2024-05-16 03:24:16 +0300

    Project-Final

--- CODES/SYSTICK.c
+++ CODES/SYSTICK.c
@@ -15,24 +15,3 @@ void delay(int t);
 ///////////////////////////////////////////////
 //    	  Engy khaled sayed 2100610			 //
 ///////////////////////////////////////////////    
-void systick_init(void) {
-
-    NVIC_ST_CTRL_R = 0;
-    NVIC_ST_RELOAD_R = 0x00FFFFFF;
-    NVIC_ST_CURRENT_R = 0;
-    NVIC_ST_CTRL_R = 0x05;
-}
-
-void wait_1ms(void) {
-
-    NVIC_ST_RELOAD_R = 16000 - 1;
-    NVIC_ST_CURRENT_R = 0;
-    while ((NVIC_ST_CTRL_R & 0x00010000) == 0);
-}
-
-void delay(int t) {
-
-    int i = 0;
-    for (i=0; i < t; i++)
-        wait_1ms();
-}
